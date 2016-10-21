b1ef90037c15eb69de7da325ae5586a98e08783c
root <nitin.aryal@concur.com>
2016-10-20 23:08:25 -0700

    adding first dummy unit test

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nitina on 10/20/16.
+ */
+
+
+public class UnitTest {
+
+    private UnitTest subject;
+
+    @Before
+    public void setup() {
+        subject = new UnitTest();
+    }
+
+    @Test
+    public void testDummy() {
+
+        Assert.assertEquals(true,true);
+    }
+
+}
