228ec615640693984048143317b31f29bec06d1c
Nitin Aryal <nitin.aryal@concur.com>
2017-07-15 13:00:37 -0700

    added logic to validate param in api path vs param list provided in api doc, updated api validation and some minor fix/refactor

--- src/main/java/core/dynamic/resources/SpoofApi.java
+++ src/main/java/core/dynamic/resources/SpoofApi.java
@@ -1,5 +1,7 @@
 package core.dynamic.resources;
 
+import core.domain.enums.ApiValidationStatusCode;
+import core.error.ApiValidationStatus;
 import core.exception.EpikosException;
 import core.lib.Utility;
 
@@ -8,9 +10,38 @@ import core.lib.Utility;
  */
 final public class SpoofApi extends Api{
     @Override
-    public boolean isValid() throws EpikosException {
-        return Utility.isValidMethod(getMethod()) &&
-                Utility.isValidPath(getPath()) &&
-                Utility.isValidStatusCode(getStatus());
+    public ApiValidationStatus isValid() throws EpikosException {
+        ApiValidationStatus apiValidity = new ApiValidationStatus(ApiValidationStatusCode.Valid);
+
+        ApiValidationStatus valid = Utility.isValidMethod(getMethod());
+        if(!valid.isValidStatus()) {
+            apiValidity.setDescription(valid.getDescription());
+        }
+        valid = Utility.isValidPath(getPath());
+        if(!valid.isValidStatus()){
+            apiValidity.setDescription(apiValidity.getDescription() + "\n" + valid.getDescription());
+        }
+
+        valid = Utility.isValidStatusCode(getStatus());
+        if(!valid.isValidStatus()){
+            apiValidity.setDescription(apiValidity.getDescription() + "\n" + valid.getDescription());
+        }
+
+        valid = Utility.isValidContentType(getConsume());
+        if(!valid.isValidStatus()){
+            apiValidity.setDescription(apiValidity.getDescription() + "\n" + valid.getDescription());
+        }
+
+        valid = Utility.isValidContentType(getProduce());
+        if(!valid.isValidStatus()){
+            apiValidity.setDescription(apiValidity.getDescription() + "\n" + valid.getDescription());
+        }
+
+        valid = Utility.doesPathParamsMatchWithApiPathParam(getPath(),getApiParamList());
+        if(!valid.isValidStatus()){
+            apiValidity.setDescription(apiValidity.getDescription() + "\n" + valid.getDescription());
+        }
+
+        return apiValidity;
     }
 }
